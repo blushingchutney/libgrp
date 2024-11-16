fcbb08ae170fbb78207c500784e99ce4b83a63c4
blushingchutney <blushingchutney@users.noreply.github.com>
2024-11-15 19:10:55 -0500

    Load from file

--- source/ksgrp.cxx
+++ source/ksgrp.cxx
@@ -1,6 +1,16 @@
+#include <fstream>
 #include "ksgrp.hxx"
 
-namespace ksgrp {
-    GRP::GRP() {}
-    GRP::~GRP() {}
+KSGRP::KSGRP() {}
+KSGRP::~KSGRP() {}
+
+void KSGRP::loadFromFile(const char *path) {
+    std::ifstream fp(path, std::ios::binary);
+
+    fp.read((char*)&header, sizeof(header_t));
+
+    entries.resize(header.filecount);
+    fp.read((char*)entries.data(), sizeof(entry_t) * header.filecount);
+
+    fp.close();
 }
--- source/ksgrp.hxx
+++ source/ksgrp.hxx
@@ -3,24 +3,24 @@
 #include <vector>
 #include <cstdint>
 
-namespace ksgrp {
-    struct header_t {
-        uint8_t signature[12];
-        int32_t filecount;
-    };
+class KSGRP {
+    private:
+        struct header_t {
+            uint8_t signature[12];
+            int32_t filecount;
+        };
 
-    struct entry_t {
-        uint8_t filename[12];
-        int32_t size;
-    };
+        struct entry_t {
+            uint8_t filename[12];
+            int32_t size;
+        };
 
-    class GRP {
-        private:
-            header_t header;
-            std::vector<entry_t> entries;
-            
-        public:
-            GRP();
-            ~GRP();
-    };
-}
+        header_t header;
+        std::vector<entry_t> entries;
+        
+    public:
+        KSGRP();
+        ~KSGRP();
+
+        void loadFromFile(const char *path);
+};
