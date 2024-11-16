edcad965ecc79f986307d66aa964d7d456708bb7
blushingchutney <blushingchutney@users.noreply.github.com>
2024-11-15 19:48:46 -0500

    Print index file

--- source/test/test.cxx
+++ source/test/test.cxx
@@ -1,5 +1,15 @@
 #include <ksgrp.hxx>
 
 int main() {
+    KSGRP grp;
+    grp.loadFromFile("test.grp");
+
+    std::vector<uint8_t> README_dot_md = grp.getData(grp.find("README.md"));
+    README_dot_md.push_back('\0');
+
+    printf("\n== README.md START ==\n");
+    printf("%s", README_dot_md.data());
+    printf("\n== README.md END ==\n");
+    
     return 0;
 }
